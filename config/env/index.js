e186005f051dbcbd986f6d93727757b8a0ffe413
wangxuquan <wangxuquan@meituan.com>
2018-10-09 10:36:36 +0800

    12345

--- config/env/index.js
+++ config/env/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(`./${env}`); // eslint-disable-line import/no-dynamic-require
-
+console.log(`当前运行环境-----${env}`);
 const defaults = {
   root: path.join(__dirname, '/..')
 };
