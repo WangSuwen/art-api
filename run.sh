3790f4dd4a219d89c2533cca35832ea462227017
wangxuquan <wangxuquan@meituan.com>
2019-01-17 10:18:12 +0800

    1

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+git pull origin master
 pm2 stop artApi
 pm2 delete artApi
+NODE_MODULES="node_modules"
+if [ -d $NODE_MODULES ]; then
+  rm -rf $NODE_MODULES
+fi
+echo "npm installing..."
+npm i
+echo "server starting..."
 pm2 start ./bin/server.js --name artApi --log /var/log/art-api
