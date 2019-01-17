7db231ccc6b8ffe2d1b0a432ba91a07269400274
wangxuquan <wangxuquan@meituan.com>
2019-01-17 10:26:09 +0800

    1

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git pull origin master
+NODE_MODULES="node_modules"
+DATE=$(date +%Y_%m_%d)
 touch /var/log/art-api/$DATE.log
 pm2 stop artApi
 pm2 delete artApi
-NODE_MODULES="node_modules"
 if [ -d $NODE_MODULES ]; then
   rm -rf $NODE_MODULES
 fi
