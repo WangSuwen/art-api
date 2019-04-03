33d88254771437cbf9011e56a7a959cb094e4b7c
wangxuquan <wangxuquan@meituan.com>
2019-04-03 18:12:01 +0800

    添加 类别列表 mock数据

---
+++ mock/categories.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+      "code": "7D5EB6CB2FB7BCECC288FA6984A28E80",
+      "name": "日用居家"
+  },
+  {
+      "code": "D4192B0A74A68F2B6691DFB1A045F930",
+      "name": "玩乐.生活"
+  },
+  {
+      "code": "8B371E41EC3BABAC0CE557A1B0BAE407",
+      "name": "其它"
+  },
+  {
+      "code": "388F901CA397BC871903C528AE11A78B",
+      "name": "衣装打扮"
+  },
+  {
+      "code": "972AA8FA2102E959A352D96EAE32C14D",
+      "name": "健身/运动"
+  },
+  {
+      "code": "05969B27564BC0E9B1DB832C95D60A0A",
+      "name": "单板"
+  },
+  {
+      "code": "8973C24EF754738C3233A3B49D275DCB",
+      "name": "年代/时间"
+  }
+];
+module.exports = data;
--- routes/shounaer.js
+++ routes/shounaer.js
@@ -5,6 +5,7 @@ const result = require('../util/result');
 const addRemind = require('../mock/add-remind');
 const remindList = require('../mock/remind-list');
 const remindDetail = require('../mock/remind-detail');
+const categories = require('../mock/categories');
 
 
 // POST 添加提醒
@@ -39,36 +40,7 @@ router.get('/api/app/v350/remind-dialog', (req, res) => {
 });
 // GET 添加提醒 -- 关联物品时获取 已添加的分类列表
 router.get('/api/app/v350/categories', (req, res) => {
-    result.success(res, [
-        {
-            "code": "7D5EB6CB2FB7BCECC288FA6984A28E80",
-            "name": "日用居家"
-        },
-        {
-            "code": "D4192B0A74A68F2B6691DFB1A045F930",
-            "name": "玩乐.生活"
-        },
-        {
-            "code": "8B371E41EC3BABAC0CE557A1B0BAE407",
-            "name": "其它"
-        },
-        {
-            "code": "388F901CA397BC871903C528AE11A78B",
-            "name": "衣装打扮"
-        },
-        {
-            "code": "972AA8FA2102E959A352D96EAE32C14D",
-            "name": "健身/运动"
-        },
-        {
-            "code": "05969B27564BC0E9B1DB832C95D60A0A",
-            "name": "单板"
-        },
-        {
-            "code": "8973C24EF754738C3233A3B49D275DCB",
-            "name": "年代/时间"
-        }
-    ]);
+    result.success(res, categories);
 });
 
 
