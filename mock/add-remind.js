d0897168aaefc96f1338ce0305bb3f858f5c9b6a
wangxuquan <wangxuquan@meituan.com>
2019-04-03 15:14:51 +0800

    同步部分接口的mock数据

---
+++ mock/add-remind.js
@@ -0,0 +1,5 @@
+const data = {
+  "ok": 1,
+  "remindId": "5ca4293b579e7779e661a35a"
+};
+module.exports = data;
---
+++ mock/remind-list.js
@@ -0,0 +1,96 @@
+const data = {
+  "doneCount": 1,
+  "unDoneCount": 7,
+  "page_info": {
+      "current_page": 1,
+      "page_size": 10,
+      "count": 1
+  },
+  "reminds": [
+      {
+          "timeout": true,
+          "_id": "5c9756a50455fd0b5c092dea",
+          "title": "买大白菜4",
+          "description": "",
+          "tips_time": 1553406728526,
+          "associated_object_url": "http://cdn.shouner.com/object/2712ab56-9da9-4c15-a231-eb3a77ae3f75.jpg",
+          "done": 0,
+          "first": 1,
+          "repeat": 1,
+          "associated_object_id": "5c79f511e0817d192c935b1c"
+      },
+      {
+          "timeout": true,
+          "_id": "5c9757b82eb2b62e2066d3c8",
+          "title": "买大白菜2",
+          "description": "",
+          "tips_time": 1553406728526,
+          "associated_object_url": "http://cdn.shouner.com/object/2712ab56-9da9-4c15-a231-eb3a77ae3f75.jpg",
+          "done": 0,
+          "first": 1,
+          "repeat": 0,
+          "associated_object_id": "5c79f511e0817d192c935b1c"
+      },
+      {
+          "timeout": true,
+          "_id": "5c9757c12eb2b62e2066d3c9",
+          "title": "买大白菜5",
+          "description": "",
+          "tips_time": 1553406728526,
+          "associated_object_url": "http://cdn.shouner.com/object/2712ab56-9da9-4c15-a231-eb3a77ae3f75.jpg",
+          "done": 0,
+          "first": 0,
+          "repeat": 1,
+          "associated_object_id": "5c79f511e0817d192c935b1c"
+      },
+      {
+          "timeout": true,
+          "_id": "5c97881fc497221fc878859d",
+          "title": "买大白菜6",
+          "description": "",
+          "tips_time": 1553406728526,
+          "associated_object_url": "http://cdn.shouner.com/object/2712ab56-9da9-4c15-a231-eb3a77ae3f75.jpg",
+          "done": 0,
+          "first": 0,
+          "repeat": 0,
+          "associated_object_id": "5c79f511e0817d192c935b1c"
+      },
+      {
+          "timeout": true,
+          "_id": "5c97884b53daca17fc5e64c2",
+          "title": "买大白菜7",
+          "description": "",
+          "tips_time": 1553406728526,
+          "associated_object_url": "http://cdn.shouner.com/object/2712ab56-9da9-4c15-a231-eb3a77ae3f75.jpg",
+          "done": 0,
+          "first": 1,
+          "repeat": 1,
+          "associated_object_id": "5c79f511e0817d192c935b1c"
+      },
+      {
+          "timeout": true,
+          "_id": "5c97565f0455fd0b5c092de9",
+          "title": "买大白菜1",
+          "description": "",
+          "tips_time": 1553426728526,
+          "associated_object_url": "http://cdn.shouner.com/object/2712ab56-9da9-4c15-a231-eb3a77ae3f75.jpg",
+          "done": 0,
+          "first": 1,
+          "repeat": 1,
+          "associated_object_id": "5c79f511e0817d192c935b1c"
+      },
+      {
+          "timeout": false,
+          "_id": "5c973626dd84472ce8b987ce",
+          "title": "买大白菜3",
+          "description": "",
+          "tips_time": 1556416728526,
+          "associated_object_url": "http://cdn.shouner.com/object/2712ab56-9da9-4c15-a231-eb3a77ae3f75.jpg",
+          "done": 0,
+          "first": 1,
+          "repeat": 1,
+          "associated_object_id": "5c79efbe30220b51e625674c"
+      }
+  ]
+};
+module.exports = data;
